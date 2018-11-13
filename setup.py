fd09dcb693cbedb1b601f56c3d4246a97a234b10
Dan Dedrick <ddedrick@lexmark.com>
2018-11-13 16:23:17 -0500

    setup.py: update information
    This is in prepartion for uploading to pypi.

--- setup.py
+++ setup.py
@@ -19,5 +19,13 @@ setuptools.setup(
         description='A tool for executing commands based on strace output',
         long_description=long_description,
         long_description_content_type='text/markdown',
-        packages=setuptools.find_packages()
+        packages=setuptools.find_packages(),
+        url="https://github.com/dandedrick/straceexec",
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: POSIX :: Linux",
+            "Topic :: Utilities",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3"
+            ]
         )
